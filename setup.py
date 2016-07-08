e4a660752e1079c2271cc044387e8ccb2eca6bda
tintoy <tintoy@tintoy.io>
2016-07-08 18:58:04 +1000

    v.0.04

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='seqlog',
-    version='0.0.3',
+    version='0.0.4',
     description="SeqLog enables logging from Python to Seq.",
     long_description=readme + '\n\n' + history,
     author="Adam Friedman",
