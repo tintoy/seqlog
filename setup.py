4bbbd666e77d60c0d86e213c561927a307dfa1e9
Adam Friedman <tintoy@tintoy.io>
2018-01-05 15:43:27 +1100

    Bump version: 0.3.5 → 0.3.6

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ test_requirements = [
 
 setup(
     name='seqlog',
-    version='0.3.5',
+    version='0.3.6',
     description="SeqLog enables logging from Python to Seq.",
     long_description=readme + '\n\n' + history,
     author="Adam Friedman",
