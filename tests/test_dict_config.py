26b939ae2ba2ea15b69cf103d7517f45a058bd41
Adam Friedman <tintoy@tintoy.io>
2023-10-28 10:53:58 +1100

    Ignore common flake8 errors

--- tests/test_dict_config.py
+++ tests/test_dict_config.py
@@ -45,6 +45,7 @@ TEST_CONFIG = {
     }
 }
 
+
 class TestDictConfig():
     def test_logger_with_dict_config(self):
         global TEST_CONFIG
@@ -61,8 +62,7 @@ class TestDictConfig():
         for level in test_levels.keys():
             TEST_CONFIG['loggers']['test_logger']['level'] = test_levels.get(level)
             seqlog.configure_from_dict(TEST_CONFIG)
-            
+
             for log_level in test_levels:
                 test_logger = seqlog.logging.getLogger('test_logger')
                 test_logger.log(msg=f"This is a {log_level} log sent with {level} set.", level=test_levels.get(level))
-
