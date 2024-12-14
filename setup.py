ba9d4e8e4adde83ff4d316d2b560f47da8e4e15e
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2024-12-14 22:06:55 +0100

    a doc update

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
